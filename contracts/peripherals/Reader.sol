de243a4061763992675074f076d56b0f5e9299f7
Phi <phi@lattefi.app>
2021-09-23 22:07:46 +0800

    update reader

--- contracts/peripherals/Reader.sol
+++ contracts/peripherals/Reader.sol
@@ -236,6 +236,34 @@ contract Reader {
         return amounts;
     }
 
+    function getFullVaultTokenInfo(address _vault, address _weth, uint256 _usdgAmount, address[] memory _tokens) public view returns (uint256[] memory) {
+        uint256 propsLength = 11;
+
+        IVault vault = IVault(_vault);
+        IVaultPriceFeed priceFeed = IVaultPriceFeed(vault.priceFeed());
+
+        uint256[] memory amounts = new uint256[](_tokens.length * propsLength);
+        for (uint256 i = 0; i < _tokens.length; i++) {
+            address token = _tokens[i];
+            if (token == address(0)) {
+                token = _weth;
+            }
+            amounts[i * propsLength] = vault.poolAmounts(token);
+            amounts[i * propsLength + 1] = vault.reservedAmounts(token);
+            amounts[i * propsLength + 2] = vault.usdgAmounts(token);
+            amounts[i * propsLength + 3] = vault.getRedemptionAmount(token, _usdgAmount);
+            amounts[i * propsLength + 4] = vault.tokenWeights(token);
+            amounts[i * propsLength + 5] = vault.bufferAmounts(token);
+            amounts[i * propsLength + 6] = vault.getMinPrice(token);
+            amounts[i * propsLength + 7] = vault.getMaxPrice(token);
+            amounts[i * propsLength + 8] = vault.guaranteedUsd(token);
+            amounts[i * propsLength + 9] = priceFeed.getPrice(token, false, false, false);
+            amounts[i * propsLength + 10] = priceFeed.getPrice(token, true, false, false);
+        }
+
+        return amounts;
+    }
+
     function getPositions(address _vault, address _account, address[] memory _collateralTokens, address[] memory _indexTokens, bool[] memory _isLong) public view returns(uint256[] memory) {
         uint256[] memory amounts = new uint256[](_collateralTokens.length * POSITION_PROPS_LENGTH);
 
