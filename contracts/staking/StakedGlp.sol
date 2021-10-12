f19e832aa72f1ef813ef35f70dda865bf573216f
Phi <phi@lattefi.app>
2021-10-12 10:25:11 +0800

    update glp contracts

--- contracts/staking/StakedGlp.sol
+++ contracts/staking/StakedGlp.sol
@@ -3,17 +3,12 @@
 pragma solidity 0.6.12;
 
 import "../libraries/math/SafeMath.sol";
-import "../libraries/token/IERC20.sol";
 import "../core/interfaces/IGlpManager.sol";
 
 import "./interfaces/IRewardTracker.sol";
 
-contract wGLP is IERC20 {
+contract StakedGLP {
     using SafeMath for uint256;
-
-    string public constant name = "Wrapped GLP";
-    string public constant symbol = "wGLP";
-    uint8 public constant decimals = 18;
 
     address public glp;
     IGlpManager public glpManager;
@@ -21,6 +16,8 @@ contract wGLP is IERC20 {
     address public feeGlpTracker;
 
     mapping (address => mapping (address => uint256)) public allowances;
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
     constructor(
         address _glp,
@@ -34,38 +31,30 @@ contract wGLP is IERC20 {
         feeGlpTracker = _feeGlpTracker;
     }
 
-    function totalSupply() external override view returns (uint256) {
-        return IERC20(stakedGlpTracker).totalSupply();
-    }
-
-    function balanceOf(address _account) external override view returns (uint256) {
-        return IERC20(stakedGlpTracker).balanceOf(_account);
-    }
-
-    function allowance(address _owner, address _spender) external override view returns (uint256) {
+    function allowance(address _owner, address _spender) external view returns (uint256) {
         return allowances[_owner][_spender];
     }
 
-    function approve(address _spender, uint256 _amount) external override returns (bool) {
+    function approve(address _spender, uint256 _amount) external returns (bool) {
         _approve(msg.sender, _spender, _amount);
         return true;
     }
 
-    function transfer(address _recipient, uint256 _amount) external override returns (bool) {
+    function transfer(address _recipient, uint256 _amount) external returns (bool) {
         _transfer(msg.sender, _recipient, _amount);
         return true;
     }
 
-    function transferFrom(address _sender, address _recipient, uint256 _amount) external override returns (bool) {
-        uint256 nextAllowance = allowances[_sender][msg.sender].sub(_amount, "wGLP: transfer amount exceeds allowance");
+    function transferFrom(address _sender, address _recipient, uint256 _amount) external returns (bool) {
+        uint256 nextAllowance = allowances[_sender][msg.sender].sub(_amount, "StakedGLP: transfer amount exceeds allowance");
         _approve(_sender, msg.sender, nextAllowance);
         _transfer(_sender, _recipient, _amount);
         return true;
     }
 
     function _approve(address _owner, address _spender, uint256 _amount) private {
-        require(_owner != address(0), "wGLP: approve from the zero address");
-        require(_spender != address(0), "wGLP: approve to the zero address");
+        require(_owner != address(0), "StakedGLP: approve from the zero address");
+        require(_spender != address(0), "StakedGLP: approve to the zero address");
 
         allowances[_owner][_spender] = _amount;
 
@@ -73,12 +62,12 @@ contract wGLP is IERC20 {
     }
 
     function _transfer(address _sender, address _recipient, uint256 _amount) private {
-        require(_sender != address(0), "wGLP: transfer from the zero address");
-        require(_recipient != address(0), "wGLP: transfer to the zero address");
+        require(_sender != address(0), "StakedGLP: transfer from the zero address");
+        require(_recipient != address(0), "StakedGLP: transfer to the zero address");
 
         require(
             glpManager.lastAddedAt(_sender).add(glpManager.cooldownDuration()) <= block.timestamp,
-            "wGLP: cooldown duration not yet passed"
+            "StakedGLP: cooldown duration not yet passed"
         );
 
         IRewardTracker(stakedGlpTracker).unstakeForAccount(_sender, feeGlpTracker, _amount, _sender);
@@ -86,7 +75,5 @@ contract wGLP is IERC20 {
 
         IRewardTracker(feeGlpTracker).stakeForAccount(_sender, _recipient, glp, _amount);
         IRewardTracker(stakedGlpTracker).stakeForAccount(_recipient, _recipient, feeGlpTracker, _amount);
-
-        emit Transfer(_sender, _recipient,_amount);
     }
 }
