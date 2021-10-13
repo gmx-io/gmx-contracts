38d210964d48b47f3e3bc80ad5533e45ebce9a50
Phi <phi@lattefi.app>
2021-10-13 17:43:45 +0800

    update scripts

--- scripts/staking/updateRewards.js
+++ scripts/staking/updateRewards.js
@@ -9,12 +9,12 @@ async function main() {
     {
       name: "feeGmxTracker",
       address: "0xd2D1162512F927a7e282Ef43a362659E4F2a728F",
-      transferAmount: "26"
+      transferAmount: "27"
     },
     {
       name: "feeGlpTracker",
       address: "0x4e971a87900b931fF39d1Aad67697F49835400b6",
-      transferAmount: "44"
+      transferAmount: "45"
     }
   ]
 
