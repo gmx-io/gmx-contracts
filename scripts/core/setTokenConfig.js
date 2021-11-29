a208d016254dda3be0be41e6222b14ed7c4a62c8
Phi <phi@lattefi.app>
2021-11-29 14:07:23 +0800

    update token configs

--- scripts/core/setTokenConfig.js
+++ scripts/core/setTokenConfig.js
@@ -15,8 +15,8 @@ async function main() {
   console.log("vault", vault.address)
   console.log("timelock", timelock.address)
 
-  const { btc, eth, usdc, link, uni, usdt, mim } = tokens
-  const tokenArr = [btc, eth, usdc, link, uni, usdt, mim]
+  const { btc, eth, usdc, link, uni, usdt, mim, frax } = tokens
+  const tokenArr = [btc, eth, usdc, link, uni, usdt, mim, frax]
 
   for (const token of tokenArr) {
     await sendTxn(timelock.setTokenConfig(
