1800e19d6c51da0dc1c95f4c09b731c74462b8b8
Phi <phi@lattefi.app>
2021-12-17 16:38:11 +0800

    update reader

--- contracts/peripherals/Reader.sol
+++ contracts/peripherals/Reader.sol
@@ -305,6 +305,36 @@ contract Reader {
         return amounts;
     }
 
+    function getVaultTokenInfoV2(address _vault, address _weth, uint256 _usdgAmount, address[] memory _tokens) public view returns (uint256[] memory) {
+        uint256 propsLength = 13;
+
+        IVault vault = IVault(_vault);
+        IVaultPriceFeed priceFeed = IVaultPriceFeed(vault.priceFeed());
+
+        uint256[] memory amounts = new uint256[](_tokens.length * propsLength);
+        for (uint256 i = 0; i < _tokens.length; i++) {
+            address token = _tokens[i];
+            if (token == address(0)) {
+                token = _weth;
+            }
+            amounts[i * propsLength] = vault.poolAmounts(token);
+            amounts[i * propsLength + 1] = vault.reservedAmounts(token);
+            amounts[i * propsLength + 2] = vault.usdgAmounts(token);
+            amounts[i * propsLength + 3] = vault.getRedemptionAmount(token, _usdgAmount);
+            amounts[i * propsLength + 4] = vault.tokenWeights(token);
+            amounts[i * propsLength + 5] = vault.bufferAmounts(token);
+            amounts[i * propsLength + 6] = vault.maxUsdgAmounts(token);
+            amounts[i * propsLength + 7] = vault.maxGlobalShortSizes(token);
+            amounts[i * propsLength + 8] = vault.getMinPrice(token);
+            amounts[i * propsLength + 9] = vault.getMaxPrice(token);
+            amounts[i * propsLength + 10] = vault.guaranteedUsd(token);
+            amounts[i * propsLength + 11] = priceFeed.getPrimaryPrice(token, false);
+            amounts[i * propsLength + 12] = priceFeed.getPrimaryPrice(token, true);
+        }
+
+        return amounts;
+    }
+
     function getPositions(address _vault, address _account, address[] memory _collateralTokens, address[] memory _indexTokens, bool[] memory _isLong) public view returns(uint256[] memory) {
         uint256[] memory amounts = new uint256[](_collateralTokens.length * POSITION_PROPS_LENGTH);
 
