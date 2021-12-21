87a9f8e9977650b8d5030acf8a06a8e56ec66932
Phi <phi@lattefi.app>
2021-12-21 15:10:38 +0800

    add FastPriceFeed.minBlockInterval

--- contracts/oracle/FastPriceFeed.sol
+++ contracts/oracle/FastPriceFeed.sol
@@ -29,7 +29,10 @@ contract FastPriceFeed is ISecondaryPriceFeed, Governable {
     uint256 public constant MAX_PRICE_DURATION = 30 minutes;
 
     uint256 public lastUpdatedAt;
+    uint256 public lastUpdatedBlock;
+
     uint256 public priceDuration;
+    uint256 public minBlockInterval;
 
     // volatility basis points
     uint256 public volBasisPoints;
@@ -67,6 +70,7 @@ contract FastPriceFeed is ISecondaryPriceFeed, Governable {
 
     constructor(
       uint256 _priceDuration,
+      uint256 _minBlockInterval,
       uint256 _maxDeviationBasisPoints,
       address _fastPriceEvents,
       address _admin,
@@ -74,6 +78,7 @@ contract FastPriceFeed is ISecondaryPriceFeed, Governable {
     ) public {
         require(_priceDuration <= MAX_PRICE_DURATION, "FastPriceFeed: invalid _priceDuration");
         priceDuration = _priceDuration;
+        minBlockInterval = _minBlockInterval;
         maxDeviationBasisPoints = _maxDeviationBasisPoints;
         fastPriceEvents = _fastPriceEvents;
         admin = _admin;
@@ -120,6 +125,8 @@ contract FastPriceFeed is ISecondaryPriceFeed, Governable {
     }
 
     function setPrices(address[] memory _tokens, uint256[] memory _prices) external onlyAdmin {
+        setLastUpdatedAt();
+
         for (uint256 i = 0; i < _tokens.length; i++) {
             address token = _tokens[i];
             prices[token] = _prices[i];
@@ -127,11 +134,10 @@ contract FastPriceFeed is ISecondaryPriceFeed, Governable {
               IFastPriceEvents(fastPriceEvents).emitPriceEvent(token, _prices[i]);
             }
         }
-        lastUpdatedAt = block.timestamp;
     }
 
     function setCompactedPrices(uint256[] memory _priceBitArray) external onlyAdmin {
-        lastUpdatedAt = block.timestamp;
+        setLastUpdatedAt();
 
         for (uint256 i = 0; i < _priceBitArray.length; i++) {
             uint256 priceBits = _priceBitArray[i];
@@ -208,5 +214,11 @@ contract FastPriceFeed is ISecondaryPriceFeed, Governable {
 
         if (_refPrice < fastPrice) { return _refPrice; }
         return fastPrice < minPrice ? minPrice : fastPrice;
+    }
+
+    function setLastUpdatedAt() private {
+        require(block.number.sub(lastUpdatedBlock) >= minBlockInterval, "FastPriceFeed: minBlockInterval not yet passed");
+        lastUpdatedAt = block.timestamp;
+        lastUpdatedBlock = block.number;
     }
 }
--- scripts/core/deployPriceFeed.js
+++ scripts/core/deployPriceFeed.js
@@ -58,6 +58,50 @@ async function deployPriceFeedArb() {
   await sendTxn(secondaryPriceFeed.setGov(fastPriceFeedGov.address), "secondaryPriceFeed.setGov")
 }
 
+async function deployPriceFeedAvax() {
+  // const timelock = { address: "0xAF2f95aA67476bFaFd8194e96B3b6dE77272ae2A" }
+  // const fastPriceFeedGov = { address: "0x49B373D422BdA4C6BfCdd5eC1E48A9a26fdA2F8b" }
+  // const fastPriceFeedAdmin = { address: "0x67F1B9E91D7bB46556ba013c1B187C461e2a1Ffd" }
+  const signers = ["0x8A78BA7F6c187e381ffE9B6414FC11cebd5993c1"]
+  const tokenManager = { address: "0x1EF8156b46e6f5A1973BfF4975177fd13275Ad59" }
+
+  const fastPriceEvents = await deployContract("FastPriceEvents", [])
+
+  const secondaryPriceFeed = await deployContract("FastPriceFeed", [
+    5 * 60, // _priceDuration
+    250, // _maxDeviationBasisPoints
+    fastPriceEvents.address, // _fastPriceEvents
+    fastPriceFeedAdmin.address, // _admin
+    tokenManager.address // _tokenManager
+  ])
+  await sendTxn(secondaryPriceFeed.initialize(1, signers), "secondaryPriceFeed.initialize")
+
+  await sendTxn(fastPriceEvents.setIsPriceFeed(secondaryPriceFeed.address, true), "fastPriceEvents.setIsPriceFeed")
+
+  const vaultPriceFeed = await contractAt("VaultPriceFeed", "0x131238112aa25c0D8CD237a6c384d1A86D2BB152")
+  await sendTxn(vaultPriceFeed.setSecondaryPriceFeed(secondaryPriceFeed.address), "vaultPriceFeed.setSecondaryPriceFeed")
+
+  const { avax, btc, eth, mim } = tokens
+  const tokenArr = []
+  const fastPriceTokens = [avax, btc, eth, mim]
+  if (fastPriceTokens.find(t => !t.fastPricePrecision)) {
+    throw new Error("Invalid price precision")
+  }
+
+  for (const token of tokenArr) {
+    await sendTxn(vaultPriceFeed.setTokenConfig(
+      token.address, // _token
+      token.priceFeed, // _priceFeed
+      token.priceDecimals, // _priceDecimals
+      token.isStrictStable // _isStrictStable
+    ), `vaultPriceFeed.setTokenConfig(${token.name}) ${token.address} ${token.priceFeed}`)
+  }
+
+  // await sendTxn(vaultPriceFeed.setGov(timelock.address), "vaultPriceFeed.setGov")
+  await sendTxn(secondaryPriceFeed.setTokens(fastPriceTokens.map(t => t.address), fastPriceTokens.map(t => t.fastPricePrecision)), "secondaryPriceFeed.setTokens")
+  // await sendTxn(secondaryPriceFeed.setGov(fastPriceFeedGov.address), "secondaryPriceFeed.setGov")
+}
+
 async function deployPriceFeedBsc() {
   const signers = ["0xFb11f15f206bdA02c224EDC744b0E50E46137046"]
   const fastPriceFeedGov = { address: "0x4180518618F6FAA25AFf275865523470f0fa5024" }
@@ -91,6 +135,11 @@ async function deployPriceFeedBsc() {
 async function main() {
   if (network === "bsc") {
     await deployPriceFeedBsc()
+    return
+  }
+
+  if (network === "avax") {
+    await deployPriceFeedAvax()
     return
   }
 
