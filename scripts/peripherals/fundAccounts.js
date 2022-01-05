373a2ef057b13f3f36b097ce716b02f670a114c3
Phi <phi@lattefi.app>
2022-01-05 16:11:10 +0800

    update scripts

--- scripts/peripherals/fundAccounts.js
+++ scripts/peripherals/fundAccounts.js
@@ -5,7 +5,7 @@ async function main() {
   const transfers = [
     {
       address: "0x67F1B9E91D7bB46556ba013c1B187C461e2a1Ffd", // price sender
-      amount: "2.2"
+      amount: "1.6"
     },
     {
       address: "0xd4266F8F82F7405429EE18559e548979D49160F3", // order keeper
@@ -13,7 +13,7 @@ async function main() {
     },
     {
       address: "0x44311c91008DDE73dE521cd25136fD37d616802c", // liquidator
-      amount: "0.1"
+      amount: "0.2"
     }
   ]
 
--- scripts/staking/updateRewards.js
+++ scripts/staking/updateRewards.js
@@ -11,12 +11,12 @@ async function main() {
     {
       name: "feeGmxTracker",
       address: "0xd2D1162512F927a7e282Ef43a362659E4F2a728F",
-      transferAmount: "50"
+      transferAmount: "72"
     },
     {
       name: "feeGlpTracker",
       address: "0x4e971a87900b931fF39d1Aad67697F49835400b6",
-      transferAmount: "117"
+      transferAmount: "168"
     }
   ]
 
