3d56fba7f6b7353f1eaf67887f2f8c73823e80cc
Phi <phi@lattefi.app>
2022-01-05 16:38:00 +0800

    update scripts

--- scripts/core/setFees.js
+++ scripts/core/setFees.js
@@ -6,17 +6,22 @@ const network = (process.env.HARDHAT_NETWORK || 'mainnet');
 const tokens = require('./tokens')[network];
 
 async function main() {
-  const vault = await contractAt("Vault", "0xDE3590067c811b6F023b557ed45E4f1067859663")
+  const frame = new ethers.providers.JsonRpcProvider("http://127.0.0.1:1248")
+  const signer = frame.getSigner()
+  const vault = await contractAt("Vault", "0x489ee077994B6658eAfA855C308275EAd8097C4A")
+  const timelock = await contractAt("Timelock", await vault.gov())
+  console.log("timelock", timelock.address)
 
-  await sendTxn(vault.setFees(
-    10, // _taxBasisPoints
+  await sendTxn(timelock.setFees(
+    vault.address,
+    30, // _taxBasisPoints
     5, // _stableTaxBasisPoints
     20, // _mintBurnFeeBasisPoints
     20, // _swapFeeBasisPoints
     1, // _stableSwapFeeBasisPoints
     10, // _marginFeeBasisPoints
-    toUsd(1), // _liquidationFeeUsd
-    24 * 60 * 60, // _minProfitTime
+    toUsd(5), // _liquidationFeeUsd
+    12 * 60 * 60, // _minProfitTime
     true // _hasDynamicFees
   ), "vault.setFees")
 }
