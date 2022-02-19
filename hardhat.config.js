372c26f5d01b5fef86634ca2095deb77c91f32b7
Phi <phi@lattefi.app>
2022-02-19 19:51:46 +0800

    add network config

--- hardhat.config.js
+++ hardhat.config.js
@@ -45,47 +45,47 @@ module.exports = {
     hardhat: {
       allowUnlimitedContractSize: true
     },
-    // bsc: {
-    //   url: BSC_URL,
-    //   chainId: 56,
-    //   gasPrice: 10000000000,
-    //   accounts: [BSC_DEPLOY_KEY]
-    // },
-    // testnet: {
-    //   url: BSC_TESTNET_URL,
-    //   chainId: 97,
-    //   gasPrice: 20000000000,
-    //   accounts: [BSC_TESTNET_DEPLOY_KEY]
-    // },
-    // arbitrumTestnet: {
-    //   url: ARBITRUM_TESTNET_URL,
-    //   gasPrice: 10000000000,
-    //   chainId: 421611,
-    //   accounts: [ARBITRUM_TESTNET_DEPLOY_KEY]
-    // },
-    // arbitrum: {
-    //   url: ARBITRUM_URL,
-    //   gasPrice: 30000000000,
-    //   chainId: 42161,
-    //   accounts: [ARBITRUM_DEPLOY_KEY]
-    // },
-    // avax: {
-    //   url: AVAX_URL,
-    //   gasPrice: 100000000000,
-    //   chainId: 43114,
-    //   accounts: [AVAX_DEPLOY_KEY]
-    // },
-    // polygon: {
-    //   url: POLYGON_URL,
-    //   gasPrice: 100000000000,
-    //   chainId: 137,
-    //   accounts: [POLYGON_DEPLOY_KEY]
-    // },
-    // mainnet: {
-    //   url: MAINNET_URL,
-    //   gasPrice: 50000000000,
-    //   accounts: [MAINNET_DEPLOY_KEY]
-    // }
+    bsc: {
+      url: BSC_URL,
+      chainId: 56,
+      gasPrice: 10000000000,
+      accounts: [BSC_DEPLOY_KEY]
+    },
+    testnet: {
+      url: BSC_TESTNET_URL,
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [BSC_TESTNET_DEPLOY_KEY]
+    },
+    arbitrumTestnet: {
+      url: ARBITRUM_TESTNET_URL,
+      gasPrice: 10000000000,
+      chainId: 421611,
+      accounts: [ARBITRUM_TESTNET_DEPLOY_KEY]
+    },
+    arbitrum: {
+      url: ARBITRUM_URL,
+      gasPrice: 30000000000,
+      chainId: 42161,
+      accounts: [ARBITRUM_DEPLOY_KEY]
+    },
+    avax: {
+      url: AVAX_URL,
+      gasPrice: 100000000000,
+      chainId: 43114,
+      accounts: [AVAX_DEPLOY_KEY]
+    },
+    polygon: {
+      url: POLYGON_URL,
+      gasPrice: 100000000000,
+      chainId: 137,
+      accounts: [POLYGON_DEPLOY_KEY]
+    },
+    mainnet: {
+      url: MAINNET_URL,
+      gasPrice: 50000000000,
+      accounts: [MAINNET_DEPLOY_KEY]
+    }
   },
   etherscan: {
     apiKey: SNOWTRACE_API_KEY
