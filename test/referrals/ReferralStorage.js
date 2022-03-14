fb8955a157bc8895289ecc2fae04b42ed64a70aa
Phi <phi@lattefi.app>
2022-03-14 15:59:48 +0800

    update referral storage tests

--- test/referrals/ReferralStorage.js
+++ test/referrals/ReferralStorage.js
@@ -4,7 +4,7 @@ const { deployContract } = require("../shared/fixtures")
 
 use(solidity)
 
-const { AddressZero } = ethers.constants
+const { AddressZero, HashZero } = ethers.constants
 
 describe("Vester", function () {
   const provider = waffle.provider
@@ -16,6 +16,9 @@ describe("Vester", function () {
   })
 
   it("Sets new handler", async () => {
+    await expect(referralStorage.connect(user0).setHandler(user1.address, true))
+      .to.be.revertedWith("Governable: forbidden")
+
     expect(await referralStorage.isHandler(user0.address)).to.be.false
     await expect(referralStorage.connect(user1).setHandler(user0.address, true)).to.be.revertedWith("Governable: forbidden")
     await referralStorage.setHandler(user0.address, true)
@@ -28,6 +31,9 @@ describe("Vester", function () {
   it("Activates and deactivates code", async () => {
     const code = ethers.utils.keccak256("0xFF")
 
+    await expect(referralStorage.connect(user0).setIsCodeActive(code, true))
+      .to.be.revertedWith("Governable: forbidden")
+
     expect(await referralStorage.isCodeActive(code)).to.be.false
     await expect(referralStorage.connect(user1).setIsCodeActive(code, true)).to.be.revertedWith("Governable: forbidden")
     await referralStorage.setIsCodeActive(code, true)
@@ -37,58 +43,92 @@ describe("Vester", function () {
     expect(await referralStorage.isCodeActive(code)).to.be.false
   })
 
-  it("Registers code", async () => {
+  it("setTraderReferralCode", async () => {
     const code = ethers.utils.keccak256("0xFF")
 
-    expect (await referralStorage.referralCodeOwners(code)).to.be.equal(AddressZero)
-    expect (await referralStorage.isRegistered(user0.address)).to.be.false
+    await expect(referralStorage.connect(user0).setTraderReferralCode(user1.address, code))
+      .to.be.revertedWith("ReferralStorage: forbidden")
 
-    await referralStorage.connect(user0).register(code)
+    await referralStorage.setHandler(user0.address, true)
 
-    expect (await referralStorage.referralCodeOwners(code)).to.be.equal(user0.address)
-    expect (await referralStorage.isRegistered(user0.address)).to.be.true
-  })
-
-  it("Sets referral code owner", async () => {
-    const code = ethers.utils.keccak256("0xFF")
-
-    expect(Number(await referralStorage.referrals(user1.address))).to.be.equal(0)
-    await expect(referralStorage.connect(user1).setReferral(user0.address, code)).to.be.revertedWith("ReferralStorage: forbidden")
-    await referralStorage.setHandler(user1.address, true)
-    await referralStorage.connect(user1).setReferral(user0.address, code)
-    expect(await referralStorage.referrals(user0.address)).to.be.equal(code)
-  })
-
-  it("Updates referrer address", async () => {
-    const code = ethers.utils.keccak256("0xFF")
-
-    await referralStorage.connect(user0).register(code)
-    expect (await referralStorage.referralCodeOwners(code)).to.be.equal(user0.address)
-    expect (await referralStorage.isRegistered(user0.address)).to.be.true
-
-    await expect(referralStorage.connect(user1).updateAddress(code, user2.address)).to.be.revertedWith("ReferralStorage: forbidden")
-    await referralStorage.connect(user0).updateAddress(code, user2.address)
-
-    expect (await referralStorage.referralCodeOwners(code)).to.be.equal(user2.address)
-    expect (await referralStorage.isRegistered(user2.address)).to.be.true
-    // expect (await referralStorage.isRegistered(user0.address)).to.be.false
+    expect(await referralStorage.traderReferralCodes(user1.address)).eq(HashZero)
+    await referralStorage.connect(user0).setTraderReferralCode(user1.address, code)
+    expect(await referralStorage.traderReferralCodes(user1.address)).eq(code)
   })
 
   it("Registers code", async () => {
     const code = ethers.utils.keccak256("0xFF")
 
+    expect (await referralStorage.codeOwners(code)).to.be.equal(AddressZero)
+
     await referralStorage.connect(user0).register(code)
-    await referralStorage.setHandler(user1.address, true)
-    await referralStorage.connect(user1).setReferral(user2.address, code)
 
-    const referral = await referralStorage.getReferral(user2.address)
-    expect(referral[0]).to.be.equal(code)
-    expect(Number(referral[1])).to.be.equal(0)
+    expect (await referralStorage.codeOwners(code)).to.be.equal(user0.address)
 
-    await referralStorage.setIsCodeActive(code, true)
+    await expect(referralStorage.connect(user0).register(code))
+      .to.be.revertedWith("ReferralStorage: code already exists")
 
-    const referral2 = await referralStorage.getReferral(user2.address)
-    expect(referral2[0]).to.be.equal(code)
-    expect(referral2[1]).to.be.equal(user0.address)
+    const code2 = ethers.utils.keccak256("0xFF11")
+    await referralStorage.connect(user0).register(code2)
+
+    expect (await referralStorage.codeOwners(code)).to.be.equal(user0.address)
+    expect (await referralStorage.codeOwners(code2)).to.be.equal(user0.address)
+  })
+
+  it("setCodeOwner", async () => {
+    const code = ethers.utils.keccak256("0xFF")
+
+    await referralStorage.connect(user0).register(code)
+    expect (await referralStorage.codeOwners(code)).to.be.equal(user0.address)
+
+    await expect(referralStorage.connect(user1).setCodeOwner(code, user2.address)).to.be.revertedWith("ReferralStorage: forbidden")
+    await referralStorage.connect(user0).setCodeOwner(code, user2.address)
+
+    expect (await referralStorage.codeOwners(code)).to.be.equal(user2.address)
+  })
+
+  it("govSetCodeOwner", async () => {
+    const code = ethers.utils.keccak256("0xFF")
+
+    await referralStorage.connect(user0).register(code)
+    expect (await referralStorage.codeOwners(code)).to.be.equal(user0.address)
+
+    await expect(referralStorage.connect(user1).govSetCodeOwner(code, user2.address)).to.be.revertedWith("Governable: forbidden")
+    await referralStorage.connect(wallet).govSetCodeOwner(code, user2.address)
+
+    expect (await referralStorage.codeOwners(code)).to.be.equal(user2.address)
+  })
+
+  it("getTraderReferralInfo", async () => {
+    const code = ethers.utils.keccak256("0xFF")
+
+    let info = await referralStorage.getTraderReferralInfo(user1.address)
+    expect(info[0]).eq(HashZero)
+    expect(info[1]).eq(AddressZero)
+
+    await referralStorage.setHandler(user0.address, true)
+    await referralStorage.connect(user0).setTraderReferralCode(user1.address, code)
+
+    info = await referralStorage.getTraderReferralInfo(user1.address)
+    expect(info[0]).eq(code)
+    expect(info[1]).eq(AddressZero)
+
+    await referralStorage.connect(user1).register(code)
+
+    info = await referralStorage.getTraderReferralInfo(user1.address)
+    expect(info[0]).eq(code)
+    expect(info[1]).eq(AddressZero)
+
+    await referralStorage.connect(wallet).setIsCodeActive(code, true)
+
+    info = await referralStorage.getTraderReferralInfo(user1.address)
+    expect(info[0]).eq(code)
+    expect(info[1]).eq(user1.address)
+
+    await referralStorage.connect(wallet).setIsCodeActive(code, false)
+
+    info = await referralStorage.getTraderReferralInfo(user1.address)
+    expect(info[0]).eq(code)
+    expect(info[1]).eq(AddressZero)
   })
 });
