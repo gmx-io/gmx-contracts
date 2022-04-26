5b7603ae425f2a76d7eb2e379c208ee50fb45c8c
gdev8317 <95585424+gdev8317@users.noreply.github.com>
2022-04-26 13:44:52 +0300

    add esgmx to distribution rewards script

--- scripts/referrals/get-distribution-data.js
+++ scripts/referrals/get-distribution-data.js
@@ -8,19 +8,74 @@ const ARBITRUM_SUBGRAPH_ENDPOINT = 'https://api.thegraph.com/subgraphs/name/gdev
 const AVALANCHE_SUBGRAPH_ENDPOINT = 'https://api.thegraph.com/subgraphs/name/gdev8317/gmx-avalanche-referrals-staging'
 
 const BigNumber = ethers.BigNumber
-const formatUnits = ethers.utils.formatUnits
+const { formatUnits, parseUnits } = ethers.utils
 const SHARE_DIVISOR = BigNumber.from("1000000000") // 1e9
+const BONUS_TIER = 2 // for EsGMX distributions
+const USD_DECIMALS = 30
+const GMX_DECIMALS = 18
 
-async function queryDistributionData(network, fromTimestamp, toTimestamp, account) {
-  const subgraphEndpoint = {
+function stringToFixed(s, n) {
+  return Number(s).toFixed(n)
+}
+
+function bigNumberify(n) {
+  return ethers.BigNumber.from(n)
+}
+
+function expandDecimals(n, decimals) {
+  return bigNumberify(n).mul(bigNumberify(10).pow(decimals))
+}
+
+function getSubgraphEndpoint(network) {
+  return {
     avalanche: AVALANCHE_SUBGRAPH_ENDPOINT,
     arbitrum: ARBITRUM_SUBGRAPH_ENDPOINT
   }[network]
+}
+
+async function requestSubgraph(network, query) {
+  const subgraphEndpoint = getSubgraphEndpoint(network)
 
   if (!subgraphEndpoint) {
     throw new Error("Unknown network " + network)
   }
 
+  const payload = JSON.stringify({query})
+  const res = await fetch(subgraphEndpoint, {
+    method: 'POST',
+    body: payload,
+    headers: {'Content-Type': 'application/json'}
+  })
+
+  const j = await res.json()
+  if (j.errors) {
+    throw new Error(JSON.stringify(j))
+  }
+
+  return j.data
+}
+
+async function getReferrersTiers(network) {
+  const data = await requestSubgraph(network, `{
+    referrers(first: 1000, where: { tierId_in: ["2", "1"]}) {
+      id,
+      tierId
+    }
+  }`)
+
+  return data.referrers.reduce((memo, item) => {
+    memo[item.id] = parseInt(item.tierId)
+    return memo
+  }, {})
+}
+
+async function queryDistributionData(network, fromTimestamp, toTimestamp, account, gmxPrice, esgmxRewards) {
+  if (gmxPrice) {
+    gmxPrice = parseUnits(gmxPrice, USD_DECIMALS)
+  }
+  if (esgmxRewards) {
+    esgmxRewards = parseUnits(esgmxRewards, GMX_DECIMALS)
+  }
   let referrerCondition = ""
   let referralCondition = ""
   if (account) {
@@ -30,7 +85,7 @@ async function queryDistributionData(network, fromTimestamp, toTimestamp, accoun
 
   const query = `
     {
-      referrerStats(where: {
+      referrerStats(first: 1000, where: {
         period: daily,
         timestamp_gte: ${fromTimestamp},
         timestamp_lt: ${toTimestamp},
@@ -46,7 +101,7 @@ async function queryDistributionData(network, fromTimestamp, toTimestamp, accoun
         trades
         referrer
       }
-      referralStats(where: {
+      referralStats(first: 1000, where: {
         period: daily,
         timestamp_gte: ${fromTimestamp},
         timestamp_lt: ${toTimestamp},
@@ -61,29 +116,24 @@ async function queryDistributionData(network, fromTimestamp, toTimestamp, accoun
       }
     }
   `
-  const payload = JSON.stringify({query})
-  const res = await fetch(subgraphEndpoint, {
-    method: 'POST',
-    body: payload,
-    headers: {'Content-Type': 'application/json'}
-  })
 
-  const j = await res.json()
-  if (j.errors) {
-    throw new Error(JSON.stringify(j))
-  }
-
-  const data = j.data
+  const [data, referrersTiers] = await Promise.all([
+    requestSubgraph(network, query),
+    getReferrersTiers(network)
+  ])
 
   let allReferrersRebateUsd = BigNumber.from(0)
   let totalReferralVolume = BigNumber.from(0)
+  let bonusTierReferralVolume = BigNumber.from(0)
   let totalRebateUsd = BigNumber.from(0)
   const referrersRebatesData = data.referrerStats.reduce((memo, item) => {
+    const tierId = referrersTiers[account] || 0
     memo[item.referrer] = memo[item.referrer] || {
       rebateUsd: BigNumber.from(0),
       totalRebateUsd: BigNumber.from(0),
       volume: BigNumber.from(0),
-      tradesCount: 0
+      tradesCount: 0,
+      tierId
     }
     const referrerRebatesUsd = BigNumber.from(item.totalRebateUsd).sub(BigNumber.from(item.discountUsd))
     allReferrersRebateUsd = allReferrersRebateUsd.add(referrerRebatesUsd)
@@ -96,6 +146,9 @@ async function queryDistributionData(network, fromTimestamp, toTimestamp, accoun
 
     totalRebateUsd = totalRebateUsd.add(BigNumber.from(item.totalRebateUsd))
     totalReferralVolume = totalReferralVolume.add(BigNumber.from(item.volume))
+    if (tierId === BONUS_TIER) {
+      bonusTierReferralVolume = bonusTierReferralVolume.add(BigNumber.from(item.volume))
+    }
     return memo
   }, {})
 
@@ -108,7 +161,34 @@ async function queryDistributionData(network, fromTimestamp, toTimestamp, accoun
     data.allReferrersRebateUsd = allReferrersRebateUsd
     data.account = account
     data.share = data.rebateUsd.mul(SHARE_DIVISOR).div(allReferrersRebateUsd)
+    data.esgmxUsd
   })
+  if (gmxPrice && esgmxRewards) {
+    const esgmxRewardsUsdLimit = esgmxRewards.mul(gmxPrice).div(expandDecimals(1, GMX_DECIMALS))
+    let esgmxRewardsUsdTotal = BigNumber.from(0)
+    Object.values(referrersRebatesData).forEach(data => {
+      if (data.tierId !== BONUS_TIER) {
+        return
+      }
+      data.esgmxRewardsUsd = data.volume.div(1000).div(20) // 0.1% margin fee, 0.05% of fee is EsGMX bonus rewards
+      data.esgmxRewards = data.esgmxRewardsUsd
+        .mul(expandDecimals(1, USD_DECIMALS))
+        .div(gmxPrice)
+        .div(expandDecimals(1, 12))
+      esgmxRewardsUsdTotal = esgmxRewardsUsdTotal.add(data.esgmxRewardsUsd)
+    })
+
+    if (esgmxRewardsUsdTotal.gt(esgmxRewardsUsdLimit)) {
+      const denominator = esgmxRewardsUsdTotal.mul(USD_DECIMALS).div(esgmxRewardsUsdLimit)
+      Object.values(referrersRebatesData).forEach(data => {
+        data.esgmxRewardsUsd = data.esgmxRewardsUsd.mul(USD_DECIMALS).div(denominator)
+        data.esgmxRewards = data.esgmxRewardsUsd
+          .mul(expandDecimals(1, USD_DECIMALS))
+          .div(gmxPrice)
+          .div(expandDecimals(1, 12))
+      })
+    }
+  }
 
   const output = {
     fromTimestamp,
@@ -122,21 +202,21 @@ async function queryDistributionData(network, fromTimestamp, toTimestamp, accoun
   }
   console.log("\nTotal referral volume: %s ($%s)",
     totalReferralVolume.toString(),
-    Number(formatUnits(totalReferralVolume, 30)).toFixed(4)
+    Number(formatUnits(totalReferralVolume, USD_DECIMALS)).toFixed(4)
   )
   console.log("Total fees collected from referral traders: %s ($%s)",
     totalReferralVolume.div(1000).toString(),
-    Number(formatUnits(totalReferralVolume.div(1000), 30)).toFixed(4)
+    Number(formatUnits(totalReferralVolume.div(1000), USD_DECIMALS)).toFixed(4)
   )
   console.log("Total rebates (for Affiliates + Traders): %s ($%s)",
     totalRebateUsd.toString(),
-    Number(formatUnits(totalRebateUsd, 30)).toFixed(4)
+    Number(formatUnits(totalRebateUsd, USD_DECIMALS)).toFixed(4)
   )
 
   console.log("\nReferrers (Affiliates):")
   console.log("Rebates sum: %s ($%s)",
     allReferrersRebateUsd.toString(),
-    Number(formatUnits(allReferrersRebateUsd, 30)).toFixed(4)
+    Number(formatUnits(allReferrersRebateUsd, USD_DECIMALS)).toFixed(4)
   )
   let consoleData = []
   for (const data of Object.values(referrersRebatesData)) {
@@ -145,10 +225,13 @@ async function queryDistributionData(network, fromTimestamp, toTimestamp, accoun
     }
     consoleData.push({
       referrer: data.account,
-      "share, %": formatUnits(data.share, 7),
-      "volume, $": formatUnits(data.volume, 30),
-      "rebateUsd, $": formatUnits(data.rebateUsd, 30),
-      trades: data.tradesCount
+      "share, %": stringToFixed(formatUnits(data.share, 7), 4),
+      "volume, $": stringToFixed(formatUnits(data.volume, USD_DECIMALS), 4),
+      "rebateUsd, $": stringToFixed(formatUnits(data.rebateUsd, USD_DECIMALS), 4),
+      trades: data.tradesCount,
+      tierId: data.tierId,
+      "esgmxRewards, $": data.esgmxRewardsUsd ? formatUnits(data.esgmxRewardsUsd, USD_DECIMALS) : null,
+      esgmxRewards: data.esgmxRewards ? formatUnits(data.esgmxRewards, GMX_DECIMALS) : null,
     })
     output.referrers.push({
       account: data.account,
@@ -157,6 +240,9 @@ async function queryDistributionData(network, fromTimestamp, toTimestamp, accoun
       tradesCount: data.tradesCount,
       rebateUsd: data.rebateUsd.toString(),
       totalRebateUsd: data.totalRebateUsd.toString(),
+      tierId: data.tierId,
+      esgmxRewards: data.esgmxRewards ? data.esgmxRewards.toString() : null,
+      esgmxRewardsUsd: data.esgmxRewardsUsd ? data.esgmxRewardsUsd.toString() : null,
     })
   }
   console.table(consoleData)
@@ -182,7 +268,7 @@ async function queryDistributionData(network, fromTimestamp, toTimestamp, accoun
   console.log("Referrals (Traders):")
   console.log("Discount sum: %s ($%s)",
     allReferralsDiscountUsd.toString(),
-    Number(formatUnits(allReferralsDiscountUsd, 30)).toFixed(4)
+    Number(formatUnits(allReferralsDiscountUsd, USD_DECIMALS)).toFixed(4)
   )
   consoleData = []
   for (const data of Object.values(referralDiscountData)) {
@@ -191,9 +277,9 @@ async function queryDistributionData(network, fromTimestamp, toTimestamp, accoun
     }
     consoleData.push({
       referral: data.account,
-      "share, %": formatUnits(data.share, 7),
-      "volume, $": formatUnits(data.volume, 30),
-      "discountUsd, $": formatUnits(data.discountUsd, 30),
+      "share, %": stringToFixed(formatUnits(data.share, 7), 4),
+      "volume, $": stringToFixed(formatUnits(data.volume, USD_DECIMALS), 4),
+      "discountUsd, $": stringToFixed(formatUnits(data.discountUsd, USD_DECIMALS), 4),
     })
     output.referrals.push({
       account: data.account,
@@ -214,7 +300,6 @@ async function main() {
   const parser = new ArgumentParser({
     description: 'Get distribution data'
   });
-
   parser.add_argument('-n', '--network', {
     help: 'Network: arbitrum, avalanche',
     required: true
@@ -227,8 +312,11 @@ async function main() {
     help: 'Date to. Exclusive. E.g. 2022-04-27',
     default: "2022-04-27"
   });
-  parser.add_argument('-a', '--account', {
-    help: 'Account address'
+  parser.add_argument('-a', '--account', { help: 'Account address' })
+  parser.add_argument('-g', '--gmx-price', { help: 'GMX TWAP price' })
+  parser.add_argument('-e', '--esgmx-rewards', {
+    help: 'Amount of EsGMX to distribute to Tier 3',
+    default: "5000"
   })
 
   const args = parser.parse_args()
@@ -246,7 +334,14 @@ async function main() {
      console.log("Account: %s", args.account)
   }
 
-  await queryDistributionData(args.network, fromTimestamp, toTimestamp, args.account)
+  await queryDistributionData(
+    args.network,
+    fromTimestamp,
+    toTimestamp,
+    args.account,
+    args.gmx_price,
+    args.esgmx_rewards
+  )
 }
 
 main()
