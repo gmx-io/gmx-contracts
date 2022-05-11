cb68adbb1a69c8869522328dbf433e431df3249b
Phi <phi@lattefi.app>
2022-05-11 16:52:48 +0800

    update token configs

--- scripts/core/setTokenConfig.js
+++ scripts/core/setTokenConfig.js
@@ -25,7 +25,7 @@ async function getAvaxValues(signer) {
   const reader = await contractAt("Reader", "0x2eFEE1950ededC65De687b40Fd30a7B5f4544aBd")
 
   const { avax, eth, btc, mim, usdce, usdc } = tokens
-  const tokenArr = [avax, usdc]
+  const tokenArr = [avax, eth, btc, mim, usdce, usdc]
 
   const vaultTokenInfo = await reader.getVaultTokenInfoV2(vault.address, avax.address, 1, tokenArr.map(t => t.address))
 
