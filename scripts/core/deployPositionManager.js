b86fcf7bc786305971905ce7a59b0bcdcaed7bb8
Phi <phi@lattefi.app>
2022-06-02 22:12:44 +0800

    add partner contracts

--- scripts/core/deployPositionManager.js
+++ scripts/core/deployPositionManager.js
@@ -29,11 +29,19 @@ async function getArbValues() {
   //   "0xE40bEb54BA00838aBE076f6448b27528Dd45E4F0", // Vovo BTC up vault
   //   "0x1704A75bc723A018D176Dc603b0D1a361040dF16", // Vovo BTC down vault
   // ]
+
+  // const partnerContracts = [
+  //   "0xbFbEe90E2A96614ACe83139F41Fa16a2079e8408", // Vovo GLP ETH up vault
+  //   "0x0FAE768Ef2191fDfCb2c698f691C49035A53eF0f", // Vovo GLP ETH down vault
+  //   "0x2b8E28667A29A5Ab698b82e121F2b9Edd9271e93", // Vovo GLP BTC up vault
+  //   "0x46d6dEE922f1d2C6421895Ba182120C784d986d3", // Vovo GLP BTC down vault
+  // ]
+
   const partnerContracts = [
-    "0xbFbEe90E2A96614ACe83139F41Fa16a2079e8408", // Vovo GLP ETH up vault
-    "0x0FAE768Ef2191fDfCb2c698f691C49035A53eF0f", // Vovo GLP ETH down vault
-    "0x2b8E28667A29A5Ab698b82e121F2b9Edd9271e93", // Vovo GLP BTC up vault
-    "0x46d6dEE922f1d2C6421895Ba182120C784d986d3", // Vovo GLP BTC down vault
+    "0xC8d6d21995E00e17c5aaF07bBCde43f0ccd12725", // Jones ETH Hedging
+    "0xe36fA7dC99658C9B7E247471261b65A88077D349", // Jones gOHM Hedging
+    "0xB9bd050747357ce1fF4eFD314012ca94C07543E6", // Jones DPX Hedging
+    "0xe98f68F3380c990D3045B4ae29f3BCa0F3D02939", // Jones rDPX Hedging
   ]
 
   return { vault, timelock, router, weth, depositFee, orderBook, orderKeeper, liquidator, partnerContracts }
