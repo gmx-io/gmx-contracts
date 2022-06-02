f6b6e4dea5fb679ad622bd42f3acc4410b67de38
Phi <phi@lattefi.app>
2022-06-02 08:37:43 +0800

    add label for token manager address

--- scripts/access/deployGmxTokenManager.js
+++ scripts/access/deployGmxTokenManager.js
@@ -7,7 +7,7 @@ async function main() {
     "0x3D850Acfaa18c58b383fCA69d4d867Dc5Bb697c5", // Ben Simon
     "0x45e48668F090a3eD1C7961421c60Df4E66f693BD", // Dovey
     "0x881690382102106b00a99E3dB86056D0fC71eee6", // Han Wen
-    "0xd6D5a4070C7CFE0b42bE83934Cc21104AbeF1AD5"
+    "0xd6D5a4070C7CFE0b42bE83934Cc21104AbeF1AD5" // Bybit Security Team
   ]
 
   await sendTxn(tokenManager.initialize(signers), "tokenManager.initialize")
