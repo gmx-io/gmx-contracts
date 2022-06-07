7d0c634821ccdafc939870772a186bf8f57535af
Phi <phi@lattefi.app>
2022-06-07 16:12:07 +0800

    update gmx token managers

--- scripts/peripherals/deployGmxTimelock.js
+++ scripts/peripherals/deployGmxTimelock.js
@@ -8,7 +8,7 @@ async function runForArbitrum() {
   const rewardManager = { address: ethers.constants.AddressZero }
   const buffer = 24 * 60 * 60
   const longBuffer = 7 * 24 * 60 * 60
-  const tokenManager = { address: "0x500EE9D2527508617eE4B1cdd6846E18efbcbab2" }
+  const tokenManager = { address: "0xD83D2Ff29Bf856A52a997dc621e58262452b2DE2" }
   const mintReceiver = { address: "0x50F22389C10FcC3bA9B1AB9BCDafE40448a357FB" }
   const maxTokenSupply = expandDecimals("13250000", 18)
 
@@ -28,7 +28,7 @@ async function runForAvax() {
   const rewardManager = { address: ethers.constants.AddressZero }
   const buffer = 24 * 60 * 60
   const longBuffer = 7 * 24 * 60 * 60
-  const tokenManager = { address: "0xbc30049ADC73dE06D7a98A5189203aAC66B2c830" }
+  const tokenManager = { address: "0x26137dfA81f9Ac8BACd748f6A298262f11504Da9" }
   const mintReceiver = { address: "0x7F98d265Ba2609c1534D12cF6b0976505Ad7F653" }
   const maxTokenSupply = expandDecimals("13250000", 18)
 
