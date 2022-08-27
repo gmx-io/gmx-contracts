59c377d3f6e32f0908dae87dfc54a2014550ad42
X <phi@gmx.io>
2022-08-27 11:27:57 +0800

    update token config

--- scripts/core/tokens.js
+++ scripts/core/tokens.js
@@ -221,7 +221,7 @@ module.exports = {
       isStrictStable: true,
       tokenWeight: 2000,
       minProfitBps: 0,
-      maxUsdgAmount: 3.5 * 1000 * 1000,
+      maxUsdgAmount: 5 * 1000 * 1000,
       bufferAmount: 1 * 1000 * 1000,
       isStable: true,
       isShortable: false
