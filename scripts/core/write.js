f337b5037790694e3709ee42e46dfe7fd60f0da8
X <phi@gmx.io>
2023-03-01 09:18:59 +0800

    update scripts

--- scripts/core/write.js
+++ scripts/core/write.js
@@ -2,7 +2,7 @@ const { getFrameSigner, deployContract, contractAt, sendTxn } = require("../shar
 const { expandDecimals } = require("../../test/shared/utilities")
 
 async function main() {
-  // const signer = await getFrameSigner()
+  const signer = await getFrameSigner()
 
   // const glpManager = await contractAt("GlpManager", "0x14fB4767dc9E10F96faaF37Ad24DE3E498cC344B")
   // await sendTxn(glpManager.setCooldownDuration(10 * 60), "glpManager.setCooldownDuration")
@@ -18,8 +18,11 @@ async function main() {
   // const vaultPriceFeed = await contractAt("VaultPriceFeed", "0x30333ce00AC3025276927672aAeFd80f22E89E54")
   // await sendTxn(vaultPriceFeed.setPriceSampleSpace(2), "vaultPriceFeed.setPriceSampleSpace")
 
-  const gmx = await contractAt("GMX", "0x62edc0692BD897D2295872a9FFCac5425011c661")
-  await sendTxn(gmx.approve("0x62edc0692BD897D2295872a9FFCac5425011c661", 100, { nonce: 714 }), "gmx.approve")
+  // const gmx = await contractAt("GMX", "0x62edc0692BD897D2295872a9FFCac5425011c661")
+  // await sendTxn(gmx.approve("0x62edc0692BD897D2295872a9FFCac5425011c661", 100, { nonce: 714 }), "gmx.approve")
+
+  const timelock = await contractAt("Timelock", "0xe7e740fa40ca16b15b621b49de8e9f0d69cf4858", signer)
+  await sendTxn(timelock.signalApprove("0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a", "0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a", 1), "approve")
 }
 
 main()
