31e4f8794414e4ce5ebdf8f9a18f503754a173fb
X <phi@gmx.io>
2023-05-10 08:56:28 +0800

    update fees

--- scripts/peripherals/updateFees.js
+++ scripts/peripherals/updateFees.js
@@ -12,7 +12,7 @@ const SECONDS_PER_DAY = 24 * 60 * 60
 async function getArbValues() {
   const apiKey = ARBITRUM_SERVER_ADMIN_API_KEY
   const feeUrl = ARBITRUM_FEE_URL
-  const feeUsd = "3,369,744.12"
+  const feeUsd = "2,048,125.75"
 
   return { apiKey, feeUrl, feeUsd }
 }
@@ -20,7 +20,7 @@ async function getArbValues() {
 async function getAvaxValues() {
   const apiKey = AVAX_SERVER_ADMIN_API_KEY
   const feeUrl = AVAX_FEE_URL
-  const feeUsd = "412,892.16"
+  const feeUsd = "243,246.15"
 
   return { apiKey, feeUrl, feeUsd }
 }
