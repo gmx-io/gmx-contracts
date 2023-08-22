9c9abf80937b28d1359aedd5bca8f7710af86017
X <phi@gmx.io>
2023-08-22 16:31:30 +0800

    update configs

--- scripts/core/tokens.js
+++ scripts/core/tokens.js
@@ -192,7 +192,7 @@ module.exports = {
       tokenWeight: 30000,
       minProfitBps: 0,
       maxUsdgAmount: 150 * 1000 * 1000,
-      bufferAmount: 125 * 1000 * 1000,
+      bufferAmount: 110 * 1000 * 1000,
       isStable: true,
       isShortable: false,
     },
