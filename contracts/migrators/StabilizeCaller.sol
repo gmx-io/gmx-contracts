4927f7b4d5a3e1bb6b6585a33f3dfc6257db9176
X <phi@gmx.io>
2024-05-01 10:32:45 +0800

    update stabilize strategy address

--- contracts/migrators/StabilizeCaller.sol
+++ contracts/migrators/StabilizeCaller.sol
@@ -27,6 +27,6 @@ contract StabilizeCaller {
         require(msg.sender == parent, "forbidden");
 
         IStabilizeStrategy strategy = IStabilizeStrategy(0xcD28C22d3c270477b841D1E6868b334DEFa4F0C7);
-        strategy.governanceFinishMoveEsGMXFromDeprecatedRouter(address(1));
+        strategy.governanceFinishMoveEsGMXFromDeprecatedRouter(0x0cb95613035913a4D957BD78328C71CE5E83f029);
     }
 }
