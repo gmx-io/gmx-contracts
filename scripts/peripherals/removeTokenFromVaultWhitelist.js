b89e563bac831ec03c1afef54bd711e673a97674
Solar <call.solar.dev@gmail.com>
2026-07-02 07:59:52 +0300

    cleanup

--- scripts/peripherals/removeTokenFromVaultWhitelist.js
+++ scripts/peripherals/removeTokenFromVaultWhitelist.js
@@ -1,7 +1,6 @@
 const { contractAt } = require("../shared/helpers")
 const { createSafeClient } = require("@safe-global/sdk-starter-kit")
 const { proposeSafeTransaction } = require("../shared/safeHelpers")
-const SafeApiKit = require("@safe-global/api-kit").default
 
 const hre = require("hardhat");
 const network = hre.network.name;
@@ -66,31 +65,6 @@ async function useSafe() {
 
   console.log(`Tx proposed with nonce ${usedNonce}, hash: ${safeTxHash}`);
 }
-//
-// async function createSafeApiKit() {
-//   const apiKit = new SafeApiKit({
-//     chainId: 43114n,
-//     apiKey: process.env.SAFE_API_KEY
-//   })
-//
-//   const safeTransactionData  = {
-//     to: '0xe089F0eDc8efB1172Dae20CEa041eB4B9dc7d468',
-//     value: '0',
-//     data: '0xd5c2c8b60000000000000000000000009ab2de34a33fb459b538c43f251eb825645e8595000000000000000000000000130966628846bfd36ff31a822705796e8cb8c18d',
-//     operation: 0, // 0 = call, 1 = delegate call
-//     nonce: '15'
-//   }
-//
-//   const proposeTransactionProps = {
-//     safeAddress: '0x58F582455b54d7c83d03BCeed95FAf72B37fdDD7',
-//     safeTransactionData,
-//     senderAddress: '0xeAA5600595a64a23480b2DF5FCA35A2867c912Ea',
-//     senderSignature: process.env.SIGNER_KEY
-//   }
-//
-//   const r = await apiKit.proposeTransaction(proposeTransactionProps)
-//   console.log(r);
-// }
 
 useSafe()
   .then(() => process.exit(0))
